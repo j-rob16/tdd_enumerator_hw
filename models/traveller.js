c5e0c1982de93a348c3905cdfff2f43df57d80fe
James R <jimmer1609@gmail.com>
2022-06-23 01:21:05 +0900

    homework extensions finished

---
+++ models/traveller.js
@@ -0,0 +1,35 @@
+const Traveller = function(journeys) {
+  this.journeys = journeys;
+};
+
+Traveller.prototype.getJourneyStartLocations = function() {
+  return this.journeys.map((journey) => {
+    return journey.startLocation;
+  })
+};
+
+Traveller.prototype.getJourneyEndLocations = function () {
+  return this.journeys.map(journey => journey.endLocation);
+};
+
+Traveller.prototype.getJourneysByTransport = function (transport) {
+  return this.journeys.filter(journey => journey.transport === transport);
+};
+
+Traveller.prototype.getJourneysByMinDistance = function (minDistance) {
+  return this.journeys.filter(journey => journey.distance >= minDistance)
+};
+
+Traveller.prototype.calculateTotalDistanceTravelled = function () {
+  return this.journeys.reduce((prev, current) => {
+    return prev + current.distance;
+  }, 0);
+};
+
+Traveller.prototype.getUniqueModesOfTransport = function () {
+  const modesOfTransport = this.journeys.map(journey => journey.transport);
+  return [... new Set(modesOfTransport)]
+};
+
+
+module.exports = Traveller;
